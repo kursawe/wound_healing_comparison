8ad3722a8b51e59cc4d4c7b0559bcacdb148a96b
Jochen Kursawe <kursawe@maths.ox.ac.uk>
2018-09-12 22:26:40 +0200

    Add test for running virtual leaf

--- test/TestMakeAndCloseWound.hpp
+++ test/TestMakeAndCloseWound.hpp
@@ -1,38 +1,3 @@
-/*
-
-Copyright (c) 2005-2017, University of Oxford.
-All rights reserved.
-
-University of Oxford means the Chancellor, Masters and Scholars of the
-University of Oxford, having an administrative office at Wellington
-Square, Oxford OX1 2JD, UK.
-
-This file is part of Chaste.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice,
-   this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
- * Neither the name of the University of Oxford nor the names of its
-   contributors may be used to endorse or promote products derived from this
-   software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
-
 #ifndef TESTMAKEANDCLOSEWOUND_HPP_
 #define TESTMAKEANDCLOSEWOUND_HPP_
 #include "AbstractCellBasedWithTimingsTestSuite.hpp"
@@ -49,11 +14,13 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "FarhadifarForce.hpp"
 #include "SmartPointers.hpp"
 #include "SimpleTargetAreaModifier.hpp"
+#include "VertexMeshReader.hpp"
+#include "Debug.hpp"
 
 class TestWoundHealing : public AbstractCellBasedWithTimingsTestSuite
 {
 public:
-    void TestMakeAndCloseWound()
+    void xTestMakeAndCloseWound()
     {
         // Create a simple 2D MutableVertexMesh
         // MutableVertexMesh<2,2>* p_mesh;
@@ -113,6 +80,64 @@ public:
 
         simulator.SetDt(0.01);
 
+        // Run simulation
+        simulator.Solve();
+    }
+
+    void TestReadAndRunVirtualLeaf()
+    {
+        MARK;
+        VertexMeshReader<2,2> mesh_reader("projects/wound_healing_comparison/test/data/virtual_leaf");
+        MARK;
+        MutableVertexMesh<2,2> mesh;
+        MARK;
+        mesh.ConstructFromMeshReader(mesh_reader);
+
+        MARK;
+//        for (unsigned element_index = 0; element_index < mesh.GetNumAllElements(); element_index++)
+//        {
+//            if (mesh.GetElement(element_index)->GetNumNodes() > 12)
+//            {
+////                mesh.DeleteElementPriorToReMesh(element_index);
+//            }
+//        }
+//
+//        MARK;
+        mesh.ReMesh();
+
+        // Create cells
+        std::vector<CellPtr> cells;
+        CellsGenerator<NoCellCycleModel, 2> cells_generator;
+        cells_generator.GenerateBasic(cells, mesh.GetNumElements());
+
+        MARK;
+        for (unsigned i=0; i<cells.size(); i++)
+        {
+            cells[i]->SetBirthTime(-(double)i -19.0);
+        }
+
+        // Create cell population
+        VertexBasedCellPopulation<2> cell_population(mesh, cells);
+
+        // Set up cell-based simulation
+        OffLatticeSimulation<2> simulator(cell_population);
+        simulator.SetOutputDirectory("TestReadAndRunVirtalLeaf");
+        simulator.SetEndTime(20.0);
+        simulator.SetSamplingTimestepMultiple(10);
+        // Create a force law and pass it to the simulation
+        MAKE_PTR(FarhadifarForce<2>, p_force);
+        simulator.AddForce(p_force);
+
+        MARK;
+        // A FarhadifarForce has to be used together with an AbstractTargetAreaModifier#2488
+        MAKE_PTR(SimpleTargetAreaModifier<2>, p_growth_modifier);
+        p_growth_modifier->SetGrowthDuration(0.0);
+        simulator.AddSimulationModifier(p_growth_modifier);
+
+        MARK;
+        simulator.SetDt(0.01);
+
+        MARK;
         // Run simulation
         simulator.Solve();
     }
